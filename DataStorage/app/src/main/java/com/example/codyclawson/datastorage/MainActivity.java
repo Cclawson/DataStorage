f50a2010c6f03bd5eec862a3a1aa159c54091602
kstamant244 <kylestamant1@gmail.com>
2017-02-24 10:11:45 -0700

    Created timer class

--- DataStorage/app/src/main/java/com/example/codyclawson/datastorage/MainActivity.java
+++ DataStorage/app/src/main/java/com/example/codyclawson/datastorage/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.codyclawson.datastorage;
 
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TaskTimer taskTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create timer object with time
+        taskTimer = new TaskTimer(4000);
+        //Add listener
+
+        taskTimer.setOnTimeUpdateListener(new TaskTimer.TimerListener() {
+            @Override
+            public void OnTimeUpdate(long timeMS) {
+                System.out.println("Time(MS):"+(timeMS/1000));
+            }
+        });
+
     }
+
+    public void announceAction(String msg) {
+        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show(); // display temporary Toast message
+        System.out.println(msg);  // output to console
+    }
+
+    public void OnButtonClick(View view)
+    {
+        taskTimer.Start();
+    }
+
+
+    public void OnCancelClick(View view)
+    {
+        taskTimer.Pause();
+    }
+
+
+
 }
